80be041dd586c74cac65cffde4841046b703e379
rnd-nguyenth <nguyenth@osbholding.com>
2024-08-28 08:48:59 +0700

    latest update

---
+++ digital_economy/digital_economy/doctype/digital_economy_index/digital_economy_index.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Tran Hai Nguyen and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Digital Economy Index", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ digital_economy/digital_economy/doctype/digital_economy_index_name/test_digital_economy_index_name.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Tran Hai Nguyen and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDigitalEconomyIndexName(FrappeTestCase):
+	pass
