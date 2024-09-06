41ffc10ea2e370e893589a920953e16d47b00bf5
rnd-nguyenth <nguyenth@osbholding.com>
2024-09-06 11:41:21 +0700

    first commit

---
+++ kinhteso/kinhteso/doctype/city_industries/city_industries.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, nguyenth and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CityIndustries(Document):
+	pass
---
+++ kinhteso/kinhteso/doctype/industry/industry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, nguyenth and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Industry", {
+// 	refresh(frm) {
+
+// 	},
+// });
